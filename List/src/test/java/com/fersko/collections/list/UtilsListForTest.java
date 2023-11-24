28490e182ced2b93cae75c5242f65f560e4d2c2a
maximfersko <79246330+maximfersko@users.noreply.github.com>
2023-11-24 20:00:13 +0700

    init

--- List/src/test/java/com/fersko/collections/list/UtilsListForTest.java
+++ List/src/test/java/com/fersko/collections/list/UtilsListForTest.java
@@ -1,27 +1,6 @@
 package com.fersko.collections.list;
 
-import java.util.List;
 
 public class UtilsListForTest<T> {
-    public static <T> boolean isEqualsList(List<T> list,MyList<T> myList) {
-        if (list.size() != myList.size()) {
-            return false;
-        }
-
-        for (int i = 0; i < list.size(); i++) {
-            if (!list.get(i).equals(myList.get(i))) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    public static void  fillList(List<Integer> list, MyList<Integer> myList) {
-        for (int i = 0; i < 10; i++) {
-            list.add(i);
-            myList.add(i);
-        }
-    }
 
 }
